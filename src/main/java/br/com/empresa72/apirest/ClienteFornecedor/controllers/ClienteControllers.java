f74486fa90b0c1050e47f7370ebe69cc25f5d6c9
Acesso livre <Acesso livre@BC7N278815>
2026-06-10 19:19:19 -0300

    commit atualizar id, atualizar, deletar id

--- src/main/java/br/com/empresa72/apirest/ClienteFornecedor/controllers/ClienteControllers.java
+++ src/main/java/br/com/empresa72/apirest/ClienteFornecedor/controllers/ClienteControllers.java
@@ -1,12 +1,16 @@
 package br.com.empresa72.apirest.ClienteFornecedor.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -38,5 +42,39 @@ public class ClienteControllers {
 				return clienterep.save(cliente);
 			}
 	
+	
+	// BUSCAR POR ID //
+			@GetMapping("/listarporid/{id}")
+			@ResponseStatus(HttpStatus.OK)
+			public Optional<ClienteEntity> buscarPorID(@PathVariable Long id){
+				return clienterep.findById(id);
+					
+				}	
+	// ATUALIZAR
+			@PutMapping("/atualizar/{id}")
+			@ResponseStatus(HttpStatus.OK)
+			public String atualizarCliente(@PathVariable Long id, @RequestBody ClienteEntity cliente) {
+				if(clienterep.existsById(id)) {
+					cliente.setId(id);
+					clienterep.save(cliente);
+				return "Atualizado";
+							}
+					return "Não atualizado";
+						}		
+			
+		// DELETAR//
+			@DeleteMapping("/deletar/{id}")
+			@ResponseStatus(HttpStatus.NO_CONTENT)
+			public String deletarcliente(@PathVariable Long id) {
+				if(clienterep.existsById(id)) {
+					clienterep.deleteById(id);
+					return "Cliente deletado";
+							}
+							
+					return "Cliente não encontrado";
+						}
+			
+			
+			
 	
 }
