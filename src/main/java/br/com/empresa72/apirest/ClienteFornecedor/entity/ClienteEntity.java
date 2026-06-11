58e9b02b48edca6a83e23d8496c4e78cdf82e871
Acesso Livre <Acesso Livre@BC7N278819>
2026-06-11 20:04:43 -0300

    Merge branch 'master' of https://github.com/kallinkia/sgp_cliente_fornecedor.git

--- src/main/java/br/com/empresa72/apirest/ClienteFornecedor/entity/ClienteEntity.java
+++ src/main/java/br/com/empresa72/apirest/ClienteFornecedor/entity/ClienteEntity.java
@@ -20,6 +20,11 @@ import org.hibernate.validator.constraints.br.CPF;
 
 public class ClienteEntity implements Serializable {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
@@ -36,13 +41,21 @@ public class ClienteEntity implements Serializable {
 	private String estado;
 	private String tipo;
 	private boolean ativo;
-
+	
+	
 	// RELACIONAMENTOS
-
 	public long getId() {
 		return id;
 	}
-
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getNome_razaoSocial() {
+		return nome_razaoSocial;
+	}
+	public void setNome_razaoSocial(String nome_razaoSocial) {
+		this.nome_razaoSocial = nome_razaoSocial;
+	}
 	public String getCpf() {
 		return cpf;
 	}
@@ -55,72 +68,45 @@ public class ClienteEntity implements Serializable {
 	public void setCnpj(String cnpj) {
 		this.cnpj = cnpj;
 	}
-
-	public void setId(long id) {
-		this.id = id;
-	}
-
-	public String getNome_razaoSocial() {
-		return nome_razaoSocial;
-	}
-
-	public void setNome_razaoSocial(String nome_razaoSocial) {
-		this.nome_razaoSocial = nome_razaoSocial;
-	}
-
-
 	public String getEmail() {
 		return email;
 	}
-
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
 	public String getTelefone() {
 		return telefone;
 	}
-
 	public void setTelefone(String telefone) {
 		this.telefone = telefone;
 	}
-
 	public String getEndereco() {
 		return endereco;
 	}
-
 	public void setEndereco(String endereco) {
 		this.endereco = endereco;
 	}
-
 	public String getCidade() {
 		return cidade;
 	}
-
 	public void setCidade(String cidade) {
 		this.cidade = cidade;
 	}
-
 	public String getEstado() {
 		return estado;
 	}
-
 	public void setEstado(String estado) {
 		this.estado = estado;
 	}
-
 	public String getTipo() {
 		return tipo;
 	}
-
 	public void setTipo(String tipo) {
 		this.tipo = tipo;
 	}
-
 	public boolean isAtivo() {
 		return ativo;
 	}
-
 	public void setAtivo(boolean ativo) {
 		this.ativo = ativo;
 	}
