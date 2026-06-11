f4da195ab61ab691a4da694c39248181e72b685f
Acesso Livre <Acesso Livre@BC7N278818>
2026-06-10 21:22:33 -0300

    commit

---
+++ src/main/resources/static/js/TipoCliente.js
@@ -0,0 +1,5 @@
+const API_BUSCAR_TODOS = "http://localhost:8080/TipoCliente/listarTodos";
+const API_BUSCAR_POR_ID = "http://localhost:8080/Tipocliente/listarporid";
+const API_SALVAR = "http://localhost:8080/TipoCliente/atualizar";
+const API_ATUALIZAR = "http://localhost:8080/TipoCliente/deletar";
+const API_DELETAR = "http://localhost:8080/TipoCliente/"
